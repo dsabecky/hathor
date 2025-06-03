4766570f19d1bc100d922e056846f08a9748b69e
haldu <778367+dsabecky@users.noreply.github.com>
2025-06-03 19:06:48 -0400

    tweaks

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
-if [ ! -f "/app/data/config.py" ]; then
+if [ ! -d "/app/data" ]; then # check for config/save folder
+  printf "cannot find /app/data! Please mount a volume to /app/data"
+  exit 1
+
+elif [ ! -d "/app/db" ]; then # check for our music cache folder
+  printf "cannot find /app/db! Please mount a volume to /app/db"
+  exit 1
+
+elif [ ! -f "/app/data/config.py" ]; then # check for config file
   printf "Generating missing config.py file...\n"
   cp /app/config.py.example /app/data/config.py
-  touch /app/data/__init__.py
-
-  printf "Please edit the config.py and try again."
-
   exit 1
 fi
 
-# check for updates
-pip install --no-cache-dir -U  -r requirements.txt
+if [ -d "/app/plugins" ]; then # check for optional plugins folder
+  touch /app/plugins/__init__.py
+fi
+
+touch /app/data/__init__.py
+pip install --no-cache-dir -U  -r requirements.txt # check for updates
 
 exec "$@"
